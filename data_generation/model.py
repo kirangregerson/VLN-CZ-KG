5b205c72e7431b9c9c797d9fd471b4a10a37f972
carzh <wolfivyaura@gmail.com>
2022-12-03 13:30:55 -0500

    test version with parrot paraphrase

--- data_generation/model.py
+++ data_generation/model.py
@@ -1,27 +1,25 @@
 import torch
-from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+from parrot import Parrot
 import json
 
 NUM_RETURN_SEQUENCES = 3
 NUM_BEAMS = 10
-model_name = 'tuner007/pegasus_paraphrase'
+MODEL_NAME = "prithivida/parrot_paraphraser_on_T5"
 torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(torch_device)
 
 def create_model_and_tokenizer(): 
-    tokenizer = PegasusTokenizer.from_pretrained(model_name)
-    model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
-    return model, tokenizer
+    return Parrot(model_tag=MODEL_NAME, use_gpu=False)
 
-def get_response(input_text, model, tokenizer):
-  batch = tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
-  translated = model.generate(**batch,max_length=60,num_beams=NUM_BEAMS, num_return_sequences=NUM_RETURN_SEQUENCES, temperature=1.5)
-  tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-  return tgt_text
+def get_response(input_text, model, max_length):
+    return model.augment(input_phrase = input_text,
+                         diversity_ranker='levenshtein',
+                         do_diverse=False,
+                         max_return_phrases = NUM_RETURN_SEQUENCES,
+                         fluency_threshold = .90)
 
 def load_dataset(path_to_data):
     new_data = []
-    # lines = open(path_to_data).read().splitlines()
     for line in open(path_to_data):
         new_data.append(json.loads(line))
     return new_data
@@ -41,7 +39,7 @@ def filter_for_language(data, language):
     """
     return list(filter(lambda annotation: annotation['language'] == language, data))
 
-def write_and_generate_parallel_for_single_annotation(annotation, model, tokenizer, f):
+def write_and_generate_parallel_for_single_annotation(annotation, model, f, pad_to_len):
     """
     Parameters
     ----------
@@ -49,33 +47,52 @@ def write_and_generate_parallel_for_single_annotation(annotation, model, tokeniz
                  Represents a single annotation
     model      :
     tokenizer  :
+    f          : pointer to open file
+    pad_to_len : length to pad sequences to
 
     Returns an Array[Dictionary] of the synthetically generated annotations. These annotations
     should be identical to the original annotation besides the instruction field, which should
     include the synthetically generated data.
     """
     instruction = annotation['instruction']
-    new_instructions = get_response(instruction, model, tokenizer)
+    new_instructions = get_response(instruction, model, pad_to_len)
+    print()
+    print('old instruction: ', instruction)
 
     for new_instruction in new_instructions:
+        print('writing w the following new instruction:')
+        print(new_instruction)
         annotation['instruction'] = new_instruction
-        json.dump(annotation, f)
+        # json.dump(annotation, f)
 
     annotation['instruction'] = instruction
 
     return new_instructions
 
 def generate_parallel(data, write_to_path):
-    model, tokenizer = create_model_and_tokenizer()
+    """
+    Parameters
+    ----------
+    data          : Array[Dictionary]
+    write_to_path : String
+    """
+    model = create_model_and_tokenizer()
     f = open(write_to_path, 'a')
-    for annotation in data:
-        new_annotations = write_and_generate_parallel_for_single_annotation(annotation, model, tokenizer, f)
+    # for annotation in data:
+    #     new_annotations = write_and_generate_parallel_for_single_annotation(annotation, model, tokenizer, f)
+
+    pad_to_len = max([len(annotation['instruction']) for annotation in data])
+    print(pad_to_len)
+    print(pad_to_len)
+
+    for i in range(3):
+        new_annotations = write_and_generate_parallel_for_single_annotation(data[i], model, f, pad_to_len)
     
     f.close()
     print('done writing and generating!')
 
 
 if __name__ == '__main__':
-    new_data = load_dataset('../../rxr_train_guide')
+    new_data = load_dataset('../../rxr_train_guide.jsonl')
     filtered = filter_for_language(new_data, 'en-US')
-    generate_parallel(filtered, 'rxr_train_new')
+    generate_parallel(filtered, 'rxr_train_new.jsonl')
