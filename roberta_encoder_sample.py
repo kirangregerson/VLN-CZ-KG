10b2b882f8e6c9afba81e0a30fb5fad8e60440b6
carzh <wolfivyaura@gmail.com>
2022-12-17 00:40:58 -0500

    removed unused files

--- roberta_encoder_sample.py
+++
@@ -1,7 +0,0 @@
-from transformers import RobertaTokenizer, RobertaModel
-tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-model = RobertaModel.from_pretrained('roberta-base')
-text = "Go forward until you reach the wine cabinet."
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
-print(model)
